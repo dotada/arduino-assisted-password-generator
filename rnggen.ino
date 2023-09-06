6a5f501be8c34add7e2bf5b31508b45c0c48b2b6
dotada <luboslav.blazhev@gmail.com>
2023-09-06 21:30:31 +0300

    yes

--- rnggen.ino
+++ rnggen.ino
@@ -11,8 +11,8 @@ int adc_key_in = 0;
 #define btnNONE 5
 int finishedscreen = false;
 unsigned long randomnum = 2;
-void(* resetFunc) (void) = 0;
 
+// THIS IS FOR V1
 int read_LCD_buttons() {
   adc_key_in = analogRead(0);
   if (adc_key_in < 50) return btnRIGHT;
@@ -23,6 +23,22 @@ int read_LCD_buttons() {
 
   return btnNONE;  // when all others fail, return this...
 }
+
+/* THI IS FOR V2
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);
+ if (adc_key_in > 1000) return btnNONE;
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 250)  return btnUP; 
+ if (adc_key_in < 450)  return btnDOWN; 
+ if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;
+
+  return btnNONE;  // when all others fail, return this...
+}
+COMMENT OUT THE PREVIOUS ONE FIRST!!!!!!!!!!
+*/ 
 
 int randtempseed() {
   delay(500);
